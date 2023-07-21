c27af3f8d81d6cffdcc20464392a73ddf1244fdd
Muhammad Mahad <mahadtxt@gmail.com>
2023-07-21 10:57:03 +0500

    Added More features

--- compiler/rustc_error_codes/src/error_codes/parse.py
+++ compiler/rustc_error_codes/src/error_codes/parse.py
@@ -26,6 +26,8 @@ def find_files_with_sentence(directory, sentence):
                     matching_files.append(file_path)
     return matching_files
 
+
+
 def create_url(error_code):
     # return "https://doc.rust-lang.org/error-index.html#%s" % error_code
     # returning file with this format: [`E0139`](https://doc.rust-lang.org/error_codes/E0139.html)
@@ -41,6 +43,10 @@ if __name__ == "__main__":
     non_matching_files = find_files_without_sentence(target_directory, sentence_to_check)
     matching_files = find_files_with_sentence(target_directory, sentence_to_check)
 
+    
+
+    output_all_files = open("output_all_files.txt", "w")
+    
     # if not non_matching_files:
     # if non_matching_files:
     print("All files contain the sentence.")
@@ -55,9 +61,28 @@ if __name__ == "__main__":
             print(re.sub(target_directory, "", file_path).replace(".md", ""))
             # print(re.sub(target_directory, "", file_path))
             # print(re.sub(target_directory, "", file_path))
+        output_all_files.write(re.sub(target_directory, "", file_path).replace(".md", "") + "\n")
+    print("Files without the sentence:")
+    for file_path in non_matching_files:
+        # print(file_path)
+        # printing name of file without extension like E0001
+        print(re.sub(target_directory, "", file_path).replace(".md", ""))
+        output_all_files.write(re.sub(target_directory, "", file_path).replace(".md", "") + "\n")
+        # print(re.sub(target_directory, "", file_path))
+        # print(re.sub(target_directory, "", file_path))
+
+
     print("Total files:", len(non_matching_files) + len(matching_files))
     print("Files without the sentence:", len(non_matching_files))
     print("Files with the sentence:", len(matching_files))
+
+    # Sorting output files
+    output_all_files = open("output_all_files.txt", "r")
+    lines = output_all_files.readlines()
+    lines.sort()
+    output_all_files = open("output_all_files.txt", "w")
+    output_all_files.writelines(lines)
+    
 
     # storing this output in a file in sorted order
     output_file_with_sentence = open("output_file_with_sentence.txt", "w")
@@ -82,6 +107,38 @@ if __name__ == "__main__":
     output_file_without_sentence = open("output_file_without_sentence.txt", "w")
     output_file_without_sentence.writelines(lines)
 
+
+    # Add comment to error code output_all_files, if the error code is in output_file_with_sentence
+    output_all_files = open("output_all_files.txt", "r")
+    output_file_with_sentence = open("output_file_with_sentence.txt", "r")
+    # output_file_without_sentence = open("output_file_without_sentence.txt", "r")
+
+    lines = output_all_files.readlines()
+    lines_with_sentence = output_file_with_sentence.readlines()
+    # lines_without_sentence = output_file_without_sentence.readlines()
+
+    output_all_files = open("output_all_files.txt", "w")
+    # output_file_with_sentence = open("output_file_with_sentence.txt", "w")
+    # output_file_without_sentence = open("output_file_without_sentence.txt", "w")
+
+    for line in lines:
+        if line in lines_with_sentence:
+            output_all_files.write(line.replace("\n", "") + ", // this error code is no longer emitted by the compiler\n")
+        else:
+            output_all_files.write(line.replace("\n", "") + ",\n")
+    # for line in lines_with_sentence:
+    #     output_file_with_sentence.write(line)
+    # for line in lines_without_sentence:
+    #     output_file_without_sentence.write(line)
+
+
+
+
+    
+
+
+
+    output_all_files.close()
     output_file_with_sentence.close()
     output_file_without_sentence.close()
 
